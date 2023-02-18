5f478a6302e426524c235d3004be0619e9130e3e
saveligulas <saveli.gulas@gmail.com>
2023-02-18 11:58:35 +0100

    Initial commit

---
+++ src/main/java/gulas/saveli/securityDemo/Security/SpringSecurity.java
@@ -0,0 +1,18 @@
+package gulas.saveli.securityDemo.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SpringSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/admin").hasRole("ROLE_ADMIN")
+                .requestMatchers("/user").hasRole("ROLE_USER")
+                .requestMatchers("/").permitAll()
+                .and().formLogin();
+    }
+}
