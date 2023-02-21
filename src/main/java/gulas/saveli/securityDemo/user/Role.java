c96ef3050bd959ff67818283c8000cb009ced8a5
saveligulas <saveli.gulas@gmail.com>
2023-02-21 08:17:27 +0100

    implemented Authority in Role

--- src/main/java/gulas/saveli/securityDemo/user/Role.java
+++ src/main/java/gulas/saveli/securityDemo/user/Role.java
@@ -1,5 +1,16 @@
 package gulas.saveli.securityDemo.user;
 
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public enum Role {
-    USER, ADMIN;
+    USER(new Authority[]{Authority.USER_AUTHORITIES}), ADMIN(new Authority[]{Authority.USER_AUTHORITIES, Authority.EDIT_USERS});
+
+    private final List<Authority> AUTHORITY_LIST;
+
+    Role(Authority[] AUTHORITY_ARRAY) {
+        this.AUTHORITY_LIST = List.of(AUTHORITY_ARRAY);
+    }
 }
