51db70ad06d7dee0ebe73d4f9be128953162007c
saveligulas <saveli.gulas@gmail.com>
2023-02-21 08:20:13 +0100

    implemented Authority in User

--- src/main/java/gulas/saveli/securityDemo/user/User.java
+++ src/main/java/gulas/saveli/securityDemo/user/User.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -31,7 +32,11 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        List<SimpleGrantedAuthority> list = new ArrayList<>();
+        for(Authority authority : role.getAUTHORITY_LIST()) {
+            list.add(new SimpleGrantedAuthority(authority.toString()));
+        }
+        return list;
     }
 
     @Override
